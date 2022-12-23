6c3e46220d7bb16fd3906d8013a5e1d2fbbfbcf5
tmttaetlm <tmttaetlm@gmail.com>
2022-12-23 21:29:32 +0600

    day1 v4

--- modules/functions.py
+++ modules/functions.py
@@ -4,7 +4,7 @@ from modules.parser import get_currency_rate
 
 def currency_rate_msg(city):
     currency = get_currency_rate(city)
-    msg = f'Курс валют в городе {city} на {datetime.now().strftime("%d.%m.%Y %H:%M:%S")}\n'
+    msg = f'Курс валют в городе {city} на {datetime.now().strftime("%d.%m.%Y %H:%M:%S")}\n\n'
     cols = ['Валюта', 'Покупка', 'Продажа']
     msg += tabulate(currency, headers=cols, stralign='right', colalign=('left',))
     return '<pre>'+msg+'</pre>'
