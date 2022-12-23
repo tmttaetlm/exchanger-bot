6ce1fde5936095130946afe6b8751da93de80fcf
tmttaetlm <tmttaetlm@gmail.com>
2022-12-23 20:44:50 +0600

    day 1

--- modules/callback_handlers.py
+++ modules/callback_handlers.py
@@ -1,4 +1,4 @@
-from datetime import timedelta
+from tabulate import tabulate
 from modules.keyboards import keyboard
 from modules.parser import get_currency_rate
 from modules.mongo import get_collection
@@ -8,5 +8,8 @@ def callback_handler(bot, callback):
         city = callback.data[callback.data.index('_')+1:len(callback.data)]
         collection = get_collection('users')
         collection.update_one({'id': callback.from_user.id}, {'$set': {'city': city }})
-        bot.send_message(callback.from_user.id, get_currency_rate(city))
+        currency = get_currency_rate(city)
+        cols = ['Валюта', 'Покупка', 'Продажа']
+        msg = tabulate(currency, headers=cols, stralign='right', colalign=('left',))
+        bot.send_message(callback.from_user.id, '<pre>'+msg+'</pre>', parse_mode='HTML')
     
