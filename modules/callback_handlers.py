87c2fbde8d8ceb8f772dc2db2ae051797b3d920b
tmttaetlm <tmttaetlm@gmail.com>
2022-12-23 21:43:21 +0600

    day1 v4

--- modules/callback_handlers.py
+++ modules/callback_handlers.py
@@ -6,8 +6,6 @@ def callback_handler(bot, callback):
     if callback.data.startswith('city_'):
         city = callback.data[callback.data.index('_')+1:len(callback.data)]
         collection = get_collection('users')
-        bot.send_message(callback.from_user.id, currency_rate_msg(city), parse_mode='HTML')
-
         msg_id = collection.find_one({'id': callback.from_user.id})['msg_id']
         try: bot.delete_message(callback.from_user.id, msg_id)
         except: pass
