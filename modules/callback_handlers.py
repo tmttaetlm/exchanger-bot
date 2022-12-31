d7a0347e7564384a345b2282d68c8a3d2d6d276b
tmttaetlm <tmttaetlm@gmail.com>
2022-12-31 19:04:09 +0600

    day3 v5

--- modules/callback_handlers.py
+++ modules/callback_handlers.py
@@ -3,24 +3,22 @@ from modules.keyboards import keyboard
 from modules.functions import currency_rate_msg, exchenger_list_msg
 
 def callback_handler(bot, callback):
+    collection = get_collection('users')
+    user = collection.find_one({'id': callback.from_user.id})
+
     if callback.data.startswith('city_'):
         city = callback.data[callback.data.index('_')+1:len(callback.data)]
-        collection = get_collection('users')
-        msg_id = collection.find_one({'id': callback.from_user.id})['msg_id']
-        try: bot.delete_message(callback.from_user.id, msg_id)
+        try: bot.delete_message(callback.from_user.id, user['msg_id'])
         except: pass
         collection.update_one({'id': callback.from_user.id}, {'$set': {'city': city }})
         bot.send_message(callback.from_user.id, currency_rate_msg(city), reply_markup=keyboard('mainmenu'), parse_mode='HTML')
         
     if callback.data.startswith('currency_'):
         currency = callback.data[callback.data.index('_')+1:len(callback.data)]
-        collection = get_collection('users')
-        city = collection.find_one({'id': callback.from_user.id})['city']
-        msg_id = collection.find_one({'id': callback.from_user.id})['msg_id']
-        try: bot.delete_message(callback.from_user.id, msg_id)
+        try: bot.delete_message(callback.from_user.id, user['msg_id'])
         except: pass
-        action = collection.find_one({'id': callback.from_user.id})['action']
-        currency_from = collection.find_one({'id': callback.from_user.id})['currency_from']
+        action = user['action']
+        currency_from = user['currency_from']
         if action is not None:
             if currency_from is None:
                 collection.update_one({'id': callback.from_user.id}, {'$set': {'currency_from': currency }})
@@ -29,13 +27,11 @@ def callback_handler(bot, callback):
                 collection.update_one({'id': callback.from_user.id}, {'$set': {'currency_to': currency }})
                 res = bot.send_message(callback.from_user.id, 'Введите сумму', parse_mode='HTML')
         else:
-            bot.send_message(callback.from_user.id, exchenger_list_msg(city, currency), parse_mode='HTML')
+            bot.send_message(callback.from_user.id, exchenger_list_msg(user['city'], currency), parse_mode='HTML')
 
     if callback.data.startswith('action_'):
         action = callback.data[callback.data.index('_')+1:len(callback.data)]
-        collection = get_collection('users')
-        msg_id = collection.find_one({'id': callback.from_user.id})['msg_id']
-        try: bot.delete_message(callback.from_user.id, msg_id)
+        try: bot.delete_message(callback.from_user.id, user['msg_id'])
         except: pass
         res = bot.send_message(callback.from_user.id, 'Выберите первую валюту', reply_markup=keyboard('currencies'), parse_mode='HTML')
         collection.update_one({'id': callback.from_user.id}, {'$set': {'msg_id': res.id, 'action': action }})
--- modules/message_handlers.py
+++ modules/message_handlers.py
@@ -7,8 +7,7 @@ def message_handler(bot, message):
     user = collection.find_one({'id': message.from_user.id})
 
     if message.text == 'Курсы валют в моем городе':
-        city = user['city']
-        bot.send_message(message.from_user.id, currency_rate_msg(city), parse_mode='HTML')
+        bot.send_message(message.from_user.id, currency_rate_msg(user['city']), parse_mode='HTML')
 
     if message.text == 'Обменные пункты по валюте':
         res = bot.send_message(message.from_user.id, 'Выберите валюту', reply_markup=keyboard('currencies'), parse_mode='HTML')
@@ -16,11 +15,10 @@ def message_handler(bot, message):
 
     if message.text == 'Калькулятор конвертации валют':
         res = bot.send_message(message.from_user.id, 'Выберите действие', reply_markup=keyboard('actions'), parse_mode='HTML')
-        collection.update_one({'id': message.from_user.id}, {'$set': {'msg_id': res.id }})
+        collection.update_one({'id': message.from_user.id}, {'$set': {'msg_id': res.id, 'mode': 'convert', 'step': 1 }})
 
     if message.text == 'Сменить город':
-        city = user['city']
-        res = bot.send_message(message.from_user.id, f'Ваш текущий город {city}.\nВыберите другой город', reply_markup=keyboard('cities'), parse_mode='HTML')
+        res = bot.send_message(message.from_user.id, f'Ваш текущий город {user.city}.\nВыберите другой город', reply_markup=keyboard('cities'), parse_mode='HTML')
         collection.update_one({'id': message.from_user.id}, {'$set': {'msg_id': res.id }})
 
     if user['action'] is not None and user['currency_from'] is not None and user['currency_to'] is not None:
